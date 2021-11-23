7c9d102e3257929d59be91009b2a5386d8797c08
Jaroslav Khorishchenko <websnipter@gmail.com>
2021-11-23 14:10:10 +0200

    kremen-main-web moved

--- setup_envs.sh
+++ setup_envs.sh
@@ -9,38 +9,3 @@ tee ./web/deputies/.env.prd <<EOF
 ENV=prd
 MAPS_API_KEY=AIzaSyCDfFyXGQFPxVk813wgyIOiRvOu9f3Nvno
 EOF
-
-tee ./web/equipment/.env.dev <<EOF
-ENV=dev
-MAPS_API_KEY=AIzaSyCchqTaGYBcoeQhgw11_GeTrh533dvPFpQ
-EOF
-
-tee ./web/equipment/.env.loc <<EOF
-ENV=loc
-MAPS_API_KEY=AIzaSyCchqTaGYBcoeQhgw11_GeTrh533dvPFpQ
-EOF
-
-tee ./web/equipment/.env.prd <<EOF
-ENV=prd
-MAPS_API_KEY=AIzaSyAziRMpHFIzoqER9331LMYKAr3VtHYihxA
-EOF
-
-tee ./web/transport/.env.demo <<EOF
-ENV=prd
-MAPS_API_KEY=AIzaSyCJndeCrMk6tKzyvR0O2Q_dnOvBUqdJpJ4
-EOF
-
-tee ./web/transport/.env.dev <<EOF
-ENV=dev
-MAPS_API_KEY=AIzaSyBOW1Al_t2whGurhp07c-tNEC0wibl16Fs
-EOF
-
-tee ./web/transport/.env.loc <<EOF
-ENV=loc
-MAPS_API_KEY=AIzaSyBOW1Al_t2whGurhp07c-tNEC0wibl16Fs
-EOF
-
-tee ./web/transport/.env.prd <<EOF
-ENV=prd
-MAPS_API_KEY=AIzaSyCJndeCrMk6tKzyvR0O2Q_dnOvBUqdJpJ4
-EOF
--- web/dev/README.md
+++
@@ -1,1 +0,0 @@
-# #Kremen.Dev - web

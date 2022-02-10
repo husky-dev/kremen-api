5aa97f158013256a839cd980bcadbb628307aaf8
Jaroslav Khorishchenko <websnipter@gmail.com>
2022-02-10 12:24:04 +0200

    common: new files moved to common

---
+++ common/core/log.ts
@@ -0,0 +1,71 @@
+/* eslint-disable no-console */
+import { config, LogLevel } from '@config';
+import * as Sentry from '@sentry/node';
+import { select } from '@utils';
+
+const logLevelToNum = (val: LogLevel): number =>
+  select(val, {
+    err: 0,
+    warn: 1,
+    info: 2,
+    debug: 3,
+  });
+
+const logLevelToSymbol = (val: LogLevel): string =>
+  select(val, {
+    err: 'x',
+    warn: '!',
+    info: '+',
+    debug: '-',
+  });
+
+export const Log = (m?: string) => {
+  const level = logLevelToNum(config.log.level);
+
+  interface LogOpt {
+    level: LogLevel;
+    msg: string;
+    meta?: unknown;
+  }
+
+  const logWithOpt = (opt: LogOpt) => {
+    if (level < logLevelToNum(opt.level)) return;
+    const levelPrefix = `[${logLevelToSymbol(opt.level)}]`;
+    const prefix = m ? `${levelPrefix}[${m}]` : levelPrefix;
+    const msg = `${prefix}: ${opt.msg}`;
+    switch (opt.level) {
+      case 'info':
+        return opt.meta ? console.info(msg, JSON.stringify(opt.meta)) : console.info(msg);
+      case 'err':
+        return opt.meta ? console.error(msg, JSON.stringify(opt.meta)) : console.error(msg);
+      case 'warn':
+        return opt.meta ? console.warn(msg, JSON.stringify(opt.meta)) : console.warn(msg);
+      default:
+        return opt.meta ? console.log(msg, JSON.stringify(opt.meta)) : console.log(msg);
+    }
+  };
+
+  return {
+    err: (msg: string, meta?: unknown) => {
+      logWithOpt({ msg, meta, level: 'err' });
+    },
+    warn: (msg: string, meta?: unknown) => {
+      const metaStr = meta ? `, ${JSON.stringify(meta)}` : '';
+      Sentry.captureMessage(`${msg}${metaStr}`, Sentry.Severity.Warning);
+      logWithOpt({ msg, meta, level: 'warn' });
+    },
+    info: (msg: string, meta?: unknown) => logWithOpt({ msg, meta, level: 'info' }),
+    debug: (msg: string, meta?: unknown) => logWithOpt({ msg, meta, level: 'debug' }),
+    errAndExit: (msg: string, meta?: unknown) => {
+      logWithOpt({ msg, meta, level: 'err' });
+      process.exit(1);
+    },
+    simple: (...args: unknown[]) => console.log(...args),
+    simpleAndExit: (...args: unknown[]) => {
+      console.log(...args);
+      process.exit(0);
+    },
+  };
+};
+
+export const log = Log();
---
+++ common/utils/http.ts
@@ -0,0 +1,42 @@
+import { ServerResponse } from 'http';
+import { compact } from 'lodash';
+import { send } from 'micro';
+
+export interface HttpQs {
+  [key: string]: string | string[] | undefined;
+}
+
+export const parseQueryIdsParam = (val: string): number[] =>
+  compact(
+    val.split(',').map(item => {
+      const val = parseInt(item, 10);
+      return isNaN(val) ? undefined : val;
+    }),
+  );
+
+// Responses
+
+export const sendOk = <D = unknown>(res: ServerResponse, data: D) => sendWithHeaders(res, 200, data);
+
+export const sendErr = (res: ServerResponse, status: number, code: string, message?: string) =>
+  sendWithHeaders(res, status, { code, message });
+
+export const sendNotFoundErr = (res: ServerResponse, message: string = 'Not found') => sendErr(res, 404, 'NOT_FOUND', message);
+
+export const sendParamMissedErr = (res: ServerResponse, name: string) =>
+  sendErr(res, 422, 'PARAM_MISSED', `"${name}" param missed`);
+
+export const sendWrongFormatErr = (res: ServerResponse, name: string) =>
+  sendErr(res, 422, 'WRONG_FORMAT', `Wrong "${name}" format`);
+
+export const sendInternalServerErr = (res: ServerResponse, message: string = 'Internal server error') =>
+  sendErr(res, 500, 'INTERNAL_SERVER_ERROR', message);
+
+export const sendDatasourceErr = (res: ServerResponse, message: string = 'Datasource error') =>
+  sendErr(res, 503, 'DATASOURCE_ERROR', message);
+
+const sendWithHeaders = (res: ServerResponse, code: number, data?: unknown) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Content-Type', 'application/json');
+  send(res, code, data);
+};

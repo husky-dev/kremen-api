8775795c8ac13b5b1979c6a6efec36a991235c61
Jaroslav Khorishchenko <websnipter@gmail.com>
2022-02-13 09:41:49 +0200

    Common: utils updated

--- common/src/utils/http.ts
+++ common/src/utils/http.ts
@@ -15,9 +15,11 @@ export const parseQueryIdsParam = (val: string): number[] =>
     }),
   );
 
-// Responses
+// Data
 
-export const sendOk = <D = unknown>(res: ServerResponse, data?: D) => sendWithHeaders(res, 200, data);
+export const sendOk = <D = unknown>(res: ServerResponse, data?: D, opt?: ResponseOpt) => sendWithHeaders(res, 200, data, opt);
+
+// Errors
 
 export const sendErr = (res: ServerResponse, status: number, code: string, message?: string) =>
   sendWithHeaders(res, status, { code, message });
@@ -36,8 +38,42 @@ export const sendInternalServerErr = (res: ServerResponse, message: string = 'In
 export const sendDatasourceErr = (res: ServerResponse, message: string = 'Datasource error') =>
   sendErr(res, 503, 'DATASOURCE_ERROR', message);
 
-const sendWithHeaders = (res: ServerResponse, code: number, data?: unknown) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Content-Type', 'application/json');
+// Headers
+
+interface ResponseOpt {
+  cors?: boolean;
+  cache?: {
+    type?: 'private' | 'public';
+    sec?: number;
+  };
+  contentType?: string;
+}
+
+const setContentTypeHeaders = (res: ServerResponse, opt?: ResponseOpt) => {
+  res.setHeader('Content-Type', opt?.contentType || 'application/json');
+};
+
+const setCorsHeaders = (res: ServerResponse, opt?: ResponseOpt) => {
+  if (opt?.cors !== false) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+  }
+};
+
+const setCacheHeaders = (res: ServerResponse, opt?: ResponseOpt) => {
+  if (!opt || !opt.cache) return;
+  const { cache } = opt;
+  const parts: string[] = [];
+  if (cache.type === 'private') parts.push('private');
+  else parts.push('public');
+  if (cache.sec) parts.push(`max-age=${cache.sec}`);
+  if (parts.length) res.setHeader('Cache-Control', parts.join(', '));
+};
+
+// Response
+
+const sendWithHeaders = (res: ServerResponse, code: number, data?: unknown, opt?: ResponseOpt) => {
+  setCorsHeaders(res, opt);
+  setCacheHeaders(res, opt);
+  setContentTypeHeaders(res, opt);
   send(res, code, data);
 };

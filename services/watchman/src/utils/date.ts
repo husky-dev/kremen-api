eac426f110803682ab5a8019c6a084fc530a60f9
Jaroslav Khorishchenko <websnipter@gmail.com>
2022-02-14 13:09:25 +0200

    common: date updated

--- services/watchman/src/utils/date.ts
+++ services/watchman/src/utils/date.ts
@@ -1,6 +1,8 @@
 // Milliseconds
 export const secMs = 1000;
 export const minMs = secMs * 60;
+export const hourMs = minMs * 60;
+export const dayMs = hourMs * 24;
 // Seconds
 export const minSec = 60;
 export const hourSec = minSec * 60;
